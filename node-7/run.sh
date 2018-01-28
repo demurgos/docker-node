6415baf8d6ea4b121b41219ae6eae61b789331c3
Charles Samborski <demurgos@demurgos.net>
2018-01-28 16:58:44 +0100

    Drop Node 7 and Node 8, update Node 9

--- node-7/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -e
-# Echo and run
-CMD="docker run -it demurgos/node:7" && echo "$CMD" && eval "$CMD"
--- node-8/run-gui.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -e
-# Echo and run
-CMD="xhost local:docker && docker run -it -e DISPLAY -v $HOME/.Xauthority:/home/root/.Xauthority --net=host demurgos/node:8" && echo "$CMD" && eval "$CMD"
