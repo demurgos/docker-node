ea162bdbc5fee73e6ee52d52a3ea31770c9aea19
Charles Samborski <demurgos@demurgos.net>
2018-05-10 20:58:54 +0200

    Update to Node 10

--- node-10/build.sh
+++ node-10/build.sh
@@ -4,4 +4,4 @@ set -e
 # Absolute path to directory containing this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Echo and run build command
-CMD="cd \"$SCRIPT_DIR\" && docker build --tag=\"demurgos/node:9\" ." && echo "$CMD" && eval "$CMD"
+CMD="cd \"$SCRIPT_DIR\" && docker build --tag=\"demurgos/node:10\" ." && echo "$CMD" && eval "$CMD"
