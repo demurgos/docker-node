72790e315f5a06e8ebcf810c35e6a0e3be75afc8
Charles Samborski <demurgos@demurgos.net>
2022-01-08 23:19:19 +0100

    Release 2022.0
    - Add `python3`
    - Update to Alpine 3.15.0

--- latest/run-gui.sh
+++ latest/run-gui.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Exit on error and echo
 set -ex
-xhost local:latest && docker run -it -e DISPLAY -v $HOME/.Xauthority:/home/root/.Xauthority --net=host demurgos/node:latest
+xhost local:latest && docker run -it -e DISPLAY -v $HOME/.Xauthority:/home/root/.Xauthority --net=host demurgos/node:2022.0
